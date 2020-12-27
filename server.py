97239cfce7ae54fa05ccf8fae74bda776bfe66ac
tomasz-urban <tomasz.urban.py@gmail.com>
2020-12-27 21:56:55 +0100

    del code

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect  #, send_from_directory
+from flask import Flask, render_template, request, redirect  #, send_from_directory 13123123
 import csv
 # import os
 app = Flask(__name__)
@@ -8,14 +8,6 @@ print(__name__)
 @app.route('/')
 def my_home():
     return render_template('index.html')
-
-
-def write_to_file(data):
-    with open('database.txt', mode='a') as database:
-        email = data['email']
-        subject = data['subject']
-        message = data['message']
-        file = database.write(f'\n{email}, {subject}, {message}')
 
 
 def write_to_csv(data):
